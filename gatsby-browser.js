0de6e96941c6b42af7d53b5714e0cc9976eb4815
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2021-10-14 21:58:36 +0200

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
-export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (!(`IntersectionObserver` in window)) {
-    console.log(`# IntersectionObserver is polyfilled!`);
-    return import(`intersection-observer`);
-  }
-};
